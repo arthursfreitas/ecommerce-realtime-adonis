3823ae7446f7de206e3563ae429dcc9a4b2c36a5
Arthur Freitas <arthurssfreitas@gmail.com>
2021-01-03 13:29:09 -0300

    password resets

--- app/Models/User.js
+++ app/Models/User.js
@@ -21,6 +21,14 @@ class User extends Model {
     })
   }
 
+  /**
+   * Hides the fields defined in the query return in the DB
+   */
+
+  static get hidden() {
+    return ['password']
+  }
+
   static get traits() {
     return [
       '@provider:Adonis/Acl/HasRole',
@@ -40,6 +48,14 @@ class User extends Model {
    */
   tokens() {
     return this.hasMany('App/Models/Token')
+  }
+
+  image() {
+    return this.belongsTo('App/Models/Image')
+  }
+
+  coupons() {
+    return this.belongsToMany('App/Models/Coupon')
   }
 }
 
