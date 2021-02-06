340424927063e929201a49244386addf055e77d4
arthursfreitas <arthurssfreitas@gmail.com>
2021-02-06 16:19:18 -0300

    category show & delete methods

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -41,7 +41,17 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    try {
+      const { title, description, image_id } = request.all()
+      const category = await Category.create({ title, description, image_id })
+      return response.status(201).send(category)
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Erro ao processar a sua solicitação!' })
+    }
+  }
 
   /**
    * Display a single category.
@@ -52,7 +62,10 @@ class CategoryController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show({ params, request, response, view }) {}
+  async show({ params: { id }, request, response, view }) {
+    const category = await Category.findOrFail(id)
+    return response.send(category)
+  }
 
   /**
    * Update category details.
@@ -72,7 +85,11 @@ class CategoryController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy({ params, request, response }) {}
+  async destroy({ params: { id }, request, response }) {
+    const category = await Category.findOrFail(id)
+    await category.delete()
+    return response.status(204).send()
+  }
 }
 
 module.exports = CategoryController
